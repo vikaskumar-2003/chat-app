955f69c511be925c065100d1651740c69fe7b285
vikaskumar-2003 <vikas0352004@gmail.com>
2026-01-18 13:20:55 +0530

    Delete youtube/youtube directory

--- youtube/youtube/src/utils/constant.js
+++
@@ -1,6 +0,0 @@
-export const YOUTUBE_LINK = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${process.env.REACT_APP_YOUTUBE_KEY}`
-
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
-
-
-export const Live_CHAT_COUNT=12
